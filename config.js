e5f245902c67e289e29f74fcafd0f4886dd6d354
Guido Pujadas <Guido_Pujadas@itresources.com.ar>
2019-06-26 10:56:10 -0300

    ADD - cors and changePort

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const config = {
     PORT: process.env.PORT || 3003,
     MONGO_DB: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/MARKDOWN_EDITOR_DB',
-    //SECRET_TOKEN: 'telemedicina',
   };
   module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ const api = express.Router();
 /* Controller */
 const fileController = require("../controllers/fileController");
 
+api.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 /** BEGIN ROUTES **/
 
 api.get("/files", fileController.getFiles);
